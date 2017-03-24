bf0463984b9298df996c3ae5a6558aecf4feb425
Kaleb Gilson <kgilso176@gmail.com>
2017-03-24 18:03:29 -0500

    Fixed an error and added some new classes

--- src/main/java/com/github/kraftlegos/Main.java
+++ src/main/java/com/github/kraftlegos/Main.java
@@ -1,18 +1,37 @@
 package com.github.kraftlegos;
 
 import com.github.kraftlegos.commands.Join;
+import com.github.kraftlegos.constructors.Game;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main extends JavaPlugin {
 
     private static Main instance;
+    private Set<Game> games = new HashSet<>();
 
     @Override
     public void onEnable() {
         instance = this;
+
+        getConfig().options().copyDefaults(true);
+        getConfig().options().copyHeader(true);
+        saveDefaultConfig();
+
         getCommand("join").setExecutor(new Join());
         getLogger();
     }
+
+    @Override
+    public void onDisable() {
+
+        instance = null;
+
+    }
+
+    public static Main getInstance() { return  instance; }
 
     public static Main get() {
         return instance;
---
+++ src/main/java/com/github/kraftlegos/constructors/Game.java
@@ -0,0 +1,4 @@
+package com.github.kraftlegos.constructors;
+
+public class Game {
+}
