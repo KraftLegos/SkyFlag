8ac8e62271839d01b74a7470d06a444eb689756f
Kaleb Gilson <kgilso176@gmail.com>
2017-03-24 18:35:28 -0500

    Commented out errored code (Possibly for future use) and changed players to private

--- src/main/java/com/github/kraftlegos/commands/Join.java
+++ src/main/java/com/github/kraftlegos/commands/Join.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class Join implements CommandExecutor {
 
-    int players = 0;
+    private int players;
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -24,14 +24,14 @@ public class Join implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "This minigame is currently full! Please try again later!");
         }
         Player p = (Player) sender;
-
+/*
         try {
             ArenaManager.add(p);
         } catch (IOException error) {
             p.sendMessage(ChatColor.RED + "A fatal error occured and you were not placed in the game. Please report this to a developer!");
             Bukkit.getServer().getLogger().severe(error.getMessage());
         }
-
+*/
 
         Bukkit.getServer().broadcastMessage(p.getCustomName() + ChatColor.YELLOW + " joined! (" + ChatColor.GREEN + players++ + "/16" + ChatColor.YELLOW + ")");
         return true;
