718bd3fd6acbe9a948930494a15800b85598e558
Kaleb Gilson <kgilso176@gmail.com>
2017-03-24 18:36:14 -0500

    Started making objects

--- src/main/java/com/github/kraftlegos/constructors/Game.java
+++ src/main/java/com/github/kraftlegos/constructors/Game.java
@@ -1,4 +1,24 @@
 package com.github.kraftlegos.constructors;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.Set;
+
 public class Game {
+
+    private int maxPlayers;
+    private int minPlayers;
+    private World world;
+    private Set<Location> spawnPoints;
+    private boolean isTeamGame;
+
+    private Set<GamePlayer> players;
+    private Set<GameTeam> team;
+
+    public Game(String gamename) {
+
+        return;
+    }
 }
