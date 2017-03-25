7132d9746b44fb13ae26d3112b2df36f19be1291
Kaleb Gilson <kgilso176@gmail.com>
2017-03-24 19:26:10 -0500

    Made an Enum for the gameteamstate

--- src/main/java/com/github/kraftlegos/constructors/GameTeam.java
+++
@@ -1,5 +0,0 @@
-package com.github.kraftlegos.constructors;
-
-public class GameTeam {
-
-}
---
+++ src/main/java/com/github/kraftlegos/object/GameTeam.java
@@ -0,0 +1,11 @@
+package com.github.kraftlegos.object;
+
+public class GameTeam {
+
+    private GameTeamState gameTeamState;
+
+    public enum GameTeamState {
+
+    }
+
+}
